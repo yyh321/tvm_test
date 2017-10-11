cf84708dacb662324f8faf378d3e3c93546c4857
yuyunhao <yuyunhao@tvmining.com>
2017-10-11 11:47:17 +0800

    first commit

---
+++ MyController.h
@@ -0,0 +1,13 @@
+//
+//  MyController.h
+//  T333
+//
+//  Created by 于云浩 on 2017/7/26.
+//  Copyright © 2017年 TVM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyController : UIViewController
+
+@end
---
+++ MyController.m
@@ -0,0 +1,37 @@
+//
+//  MyController.m
+//  T333
+//
+//  Created by 于云浩 on 2017/7/26.
+//  Copyright © 2017年 TVM. All rights reserved.
+//
+
+#import "MyController.h"
+
+@interface MyController ()
+
+@end
+
+@implementation MyController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
